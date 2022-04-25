40c2297867ada5dac529508544c60b133c7ebe8e
rudraverma390-youtube <80309555+rudraverma390-youtube@users.noreply.github.com>
2022-04-25 16:43:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Pragnay Singh" && b=="25-04-2022")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
